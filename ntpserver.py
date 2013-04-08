c3d8c9c7049ec0f684896de2e961d9580889493a
lichen <limifly@163.com>
2013-04-08 17:33:07 +0800

    Add thread for recv and process Add Ctrl+C control in main loop

--- ntpserver.py
+++ ntpserver.py
@@ -2,6 +2,13 @@ import datetime
 import socket
 import struct
 import time
+import Queue
+import mutex
+import threading
+import select
+
+taskQueue = Queue.Queue()
+stopFlag = False
 
 def system_to_ntp_time(timestamp):
     """Convert a system time to a NTP time.
@@ -145,14 +152,14 @@ class NTPPacket:
         self.ref_timestamp = 0
         """reference timestamp"""
         self.orig_timestamp = 0
-        self.orig_timestamp1 = 0
-        self.orig_timestamp2 = 0
+        self.orig_timestamp_high = 0
+        self.orig_timestamp_low = 0
         """originate timestamp"""
         self.recv_timestamp = 0
         """receive timestamp"""
         self.tx_timestamp = tx_timestamp
-        self.tx_timestamp1 = 0
-        self.tx_timestamp2 = 0
+        self.tx_timestamp_high = 0
+        self.tx_timestamp_low = 0
         """tansmit timestamp"""
         
     def to_data(self):
@@ -176,8 +183,9 @@ class NTPPacket:
                 self.ref_id,
                 _to_int(self.ref_timestamp),
                 _to_frac(self.ref_timestamp),
-                self.orig_timestamp1,
-                self.orig_timestamp2,
+                #Change by lichen, avoid loss of precision
+                self.orig_timestamp_high,
+                self.orig_timestamp_low,
                 _to_int(self.recv_timestamp),
                 _to_frac(self.recv_timestamp),
                 _to_int(self.tx_timestamp),
@@ -213,37 +221,95 @@ class NTPPacket:
         self.ref_id = unpacked[6]
         self.ref_timestamp = _to_time(unpacked[7], unpacked[8])
         self.orig_timestamp = _to_time(unpacked[9], unpacked[10])
-        self.orig_timestamp1 = unpacked[9]
-        self.orig_timestamp2 = unpacked[10]
+        self.orig_timestamp_high = unpacked[9]
+        self.orig_timestamp_low = unpacked[10]
         self.recv_timestamp = _to_time(unpacked[11], unpacked[12])
         self.tx_timestamp = _to_time(unpacked[13], unpacked[14])
-        self.tx_timestamp1 = unpacked[13]
-        self.tx_timestamp2 = unpacked[14]
-		
+        self.tx_timestamp_high = unpacked[13]
+        self.tx_timestamp_low = unpacked[14]
 
+    def GetTxTimeStamp(self):
+        return (self.tx_timestamp_high,self.tx_timestamp_low)
+
+    def SetOriginTimeStamp(self,high,low):
+        self.orig_timestamp_high = high
+        self.orig_timestamp_low = low
+        
+
+class RecvThread(threading.Thread):
+    def __init__(self,socket):
+        threading.Thread.__init__(self)
+        self.socket = socket
+    def run(self):
+        global taskQueue,stopFlag
+        while True:
+            if stopFlag == True:
+                print "RecvThread Ended"
+                break
+            rlist,wlist,elist = select.select([self.socket],[],[],1);
+            if len(rlist) != 0:
+                print "Received %d packets" % len(rlist)
+                for tempSocket in rlist:
+                    try:
+                        data,addr = tempSocket.recvfrom(1024)
+                        recvTimestamp = recvTimestamp = system_to_ntp_time(time.time())
+                        taskQueue.put((data,addr,recvTimestamp))
+                    except socket.error,msg:
+                        print msg;
+
+class WorkThread(threading.Thread):
+    def __init__(self,socket):
+        threading.Thread.__init__(self)
+        self.socket = socket
+    def run(self):
+        global taskQueue,stopFlag
+        while True:
+            if stopFlag == True:
+                print "WorkThread Ended"
+                break
+            try:
+                data,addr,recvTimestamp = taskQueue.get(timeout=1)
+                recvPacket = NTPPacket()
+                recvPacket.from_data(data)
+                timeStamp_high,timeStamp_low = recvPacket.GetTxTimeStamp()
+                sendPacket = NTPPacket(version=3,mode=4)
+                sendPacket.stratum = 2
+                sendPacket.poll = 10
+                '''
+                sendPacket.precision = 0xfa
+                sendPacket.root_delay = 0x0bfa
+                sendPacket.root_dispersion = 0x0aa7
+                sendPacket.ref_id = 0x808a8c2c
+                '''
+                sendPacket.ref_timestamp = recvTimestamp-5
+                sendPacket.SetOriginTimeStamp(timeStamp_high,timeStamp_low)
+                sendPacket.recv_timestamp = recvTimestamp
+                sendPacket.tx_timestamp = system_to_ntp_time(time.time())
+                socket.sendto(sendPacket.to_data(),addr)
+                print "Sended to %s:%d" % (addr[0],addr[1])
+            except Queue.Empty:
+                continue
+                
+        
 listenIp = "0.0.0.0"
 listenPort = 123
 socket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 socket.bind((listenIp,listenPort))
 print "local socket: ", socket.getsockname();
+recvThread = RecvThread(socket)
+recvThread.start()
+workThread = WorkThread(socket)
+workThread.start()
 
 while True:
-	data,addr = socket.recvfrom(1024);
-	recvTimestamp = system_to_ntp_time(time.time())
-	print "Received from: ",addr;
-	recvPacket = NTPPacket()
-	recvPacket.from_data(data)
-	sendPacket = NTPPacket(version=3,mode=4)
-	sendPacket.stratum = 2
-	sendPacket.poll = 10
-##	sendPacket.precision = 0xfa
-##	sendPacket.root_delay = 0x0bfa
-##	sendPacket.root_dispersion = 0x0aa7
-##	sendPacket.ref_id = 0x808a8c2c
-	sendPacket.ref_timestamp = recvTimestamp-5
-	sendPacket.orig_timestamp1 = recvPacket.tx_timestamp1
-	sendPacket.orig_timestamp2 = recvPacket.tx_timestamp2
-	sendPacket.recv_timestamp = recvTimestamp
-	sendPacket.tx_timestamp = system_to_ntp_time(time.time())
-	socket.sendto(sendPacket.to_data(),addr)
-	print "Sended"
+    try:
+        time.sleep(0.5)
+    except KeyboardInterrupt:
+        print "Exiting..."
+        stopFlag = True
+        recvThread.join()
+        workThread.join()
+        #socket.close()
+        print "Exited"
+        break
+        
